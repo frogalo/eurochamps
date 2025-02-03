822b33d08d4ca4870e0095cc61929b77d423bd31
KubaPC <ivo.urbanski@gmail.com>
2025-02-03 09:47:27 +0100

    added more schemas

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,13 @@
+// src/styles/GlobalStyles.js
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 600; // You can adjust the weight as needed
+  }
+`;

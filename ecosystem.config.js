e43d60bb40d7e9525cc8215f78f0b5c42434281f
KubaPC <ivo.urbanski@gmail.com>
2025-02-24 13:23:48 +0100

    Update package.json files with production scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,32 @@
-// ~/apps/eurochamps/ecosystem.config.js
 module.exports = {
-    apps: [
-        {
-            name: 'eurochamps-server',
-            cwd: './server',
-            script: 'src/app.js',
-            env: {
-                NODE_ENV: 'production',
-                PORT: 5000
-            },
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '1G'
+    apps : [{
+        name   : "eurochamps-server",
+        cwd: "./server",
+        script : "src/app.js",
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+            NODE_ENV: "development"
+        },
+        env_production: {
+            NODE_ENV: "production",
         }
-    ]
-};
+    }, {
+        name   : "eurochamps-client",
+        cwd: "./client",
+        script : "npm",
+        args: "start",
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+            NODE_ENV: "development"
+        },
+        env_production: {
+            NODE_ENV: "production",
+        }
+    }]
+}

00cf40603c7c29659f690935272c3380e7d1c900
KubaPC <ivo.urbanski@gmail.com>
2025-02-24 11:57:02 +0100

    Initial commit fixing

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+// ~/apps/eurochamps/ecosystem.config.js
+module.exports = {
+    apps: [
+        {
+            name: 'eurochamps-server',
+            cwd: './server',
+            script: 'src/app.js',
+            env: {
+                NODE_ENV: 'production',
+                PORT: 5000
+            },
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G'
+        }
+    ]
+};
